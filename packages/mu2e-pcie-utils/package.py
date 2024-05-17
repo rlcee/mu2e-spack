4b7227eb7fd479cef4f58f7c3cccac613426eae3
gianipez <gianipez@fnal.gov>
2024-05-17 11:48:33 -0500

    Add ryan test version

--- packages/mu2e-pcie-utils/package.py
+++ packages/mu2e-pcie-utils/package.py
@@ -25,6 +25,7 @@ class Mu2ePcieUtils(CMakePackage):
 
     license("BSD")
 
+    version("ryan_test", commit="112bf71f774d02dbee316232c82250beb4306885")
     version("develop", branch="develop", get_full_repo=True)
     version("v3_00_00", commit="757b66cb792ff5dae6bb1cdff566b60d6cfcfdc0")
     version("v2_09_01", sha256="de7debf74d81739dbfe3e7d4a7365a4c57ed3150ffd49fb5b5504f410dd469c6")
@@ -51,7 +52,8 @@ class Mu2ePcieUtils(CMakePackage):
 
     depends_on("cetmodules", type="build")
     depends_on("messagefacility")
-    depends_on("artdaq-core-mu2e@v3_00_00:", when="@develop")
+    depends_on("artdaq-core-mu2e@develop", when="@ryan_test")
+    depends_on("artdaq-core-mu2e@develop", when="@develop")
     depends_on("artdaq-core-mu2e@v3_00_00:", when="@v3_00_00:")
     depends_on("artdaq-core-mu2e@v2_00_00:v2_01_03", when="@v2_09_01")
     depends_on("trace")
