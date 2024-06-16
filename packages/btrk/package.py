3f4d54c915be508d19aa016e61aefb73cb66cd68
Ray Culbertson <rlc@fnal.gov>
2024-06-16 13:04:39 -0500

    small cleanups, add current version

--- packages/btrk/package.py
+++ packages/btrk/package.py
@@ -5,7 +5,6 @@
 
 from spack.package import *
 import os
-import shutil
 
 
 class Btrk(SConsPackage):
@@ -15,6 +14,7 @@ class Btrk(SConsPackage):
     homepage = "https://github.com/KFTrack/Btrk#readme"
     url = "https://github.com/KFTrack/BTrk/archive/refs/tags/v1_02_43.tar.gz"
 
+    version("1.02.47", sha256="ec581e62a20049da9939b8a62df866a00a9be3298d223fdda1b27d39f11f9928")
     version("1.02.46", sha256="5cc136cef7835a9cc792cd857b6fb6e94e788fd1ace07938d4599562e6740e4d")
     version("1.02.45", sha256="59d7c5afdedadb774c8a2c5ba3bbbe003d7cb09510c7a44d280cef4f511059a8")
     version("1.02.43", sha256="55eb787f156f380c80240b9f91d99963db47f015988d4cbdd3ee09e95af7759e")
@@ -51,7 +51,7 @@ class Btrk(SConsPackage):
         return args
 
     def install(self, spec, prefix):
-        move('%s/lib' % self.stage.source_path, prefix.lib)
+        install_tree('%s/lib' % self.stage.source_path, prefix.lib)
         install_tree(self.stage.source_path, prefix.source)
         mkdirp(prefix.include)
         headerlist = find_all_headers(self.stage.source_path)
@@ -69,8 +69,3 @@ class Btrk(SConsPackage):
         env.set("ROOT_INC", "%s/include/root" % self.spec["root"].prefix)
         env.set("GCC_VERSION", "{}".format(self.spec.compiler.version))
         env.set("DEBUG_LEVEL", "prof")
-        env.set("BTRK_INC", prefix.include)
-
-    def setup_dependent_run_environment(self, env, dependent_spec):
-        prefix = self.prefix
-        env.set("BTRK_INC", prefix.include)
