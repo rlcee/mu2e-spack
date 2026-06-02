8d0d080a3a59eeec052c1ece5f603485fdb21a14
Ray Culbertson <rlc@fnal.gov>
2026-06-02 08:06:25 -0500

    kinkal 3.6.0

--- packages/Offline/package.py
+++ packages/Offline/package.py
@@ -76,8 +76,8 @@ class Offline(CMakePackage):
     depends_on("kinkal@3.1.5", when="@11.05.01")
     depends_on("kinkal@3.2.1", when="@12.05.00")
     depends_on("kinkal@3.5.1", when="@13.00.08")
-    depends_on("kinkal@3.5.1,develop", when="@develop")  # UPDATE AS NEEDED
-    depends_on("kinkal@3.5.1,main", when="@main")  # UPDATE AS NEEDED
+    depends_on("kinkal@3.6.0,develop", when="@develop")  # UPDATE AS NEEDED
+    depends_on("kinkal@3.6.0,main", when="@main")  # UPDATE AS NEEDED
 
     depends_on("btrk")
     depends_on("gallery")
